b7514c448b15eceb58b5b0339e6d62011c65ed27
Kacper <kacper.blazej.sokol@cern.ch>
2014-06-30 15:44:34 +0200

    Streaming trend

--- src/testRun/RandomEventGenerator.java
+++ src/testRun/RandomEventGenerator.java
@@ -16,6 +16,10 @@ public class RandomEventGenerator {
     // frame window --- 1 or 2 depending on initialization
     private static int frame = 1;
     
+    // define mean and std for normal distribution
+    private static int mean = 50;
+    private static int std = 100;
+    
     public static void GenerateRandomTick(EPRuntime cepRT) {
  
         double price = (double) generator.nextInt(10);
@@ -37,5 +41,41 @@ public class RandomEventGenerator {
         previousPrice = (int)price;
 
     }
-	
+    
+    
+    // stream generators
+    public static void stream1() {
+    	while( true ) {
+    		double s1 = (double) generator.nextGaussian() * (double)std + (double)mean;
+    		if( s1 > 3*(double)mean/4) {
+    			// send signal
+    			double signal1 = (double) generator.nextInt(10);
+    		}
+    		//Pause for 4 seconds
+            try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    }
+    public static void stream2() {
+    	while( true ) {
+    		double s2 = (double) generator.nextGaussian() * (double)std*2 + (double)mean*2;
+    		if( s2 > 3*(double)mean*2/4) {
+    			// send signal
+    			double signal2 = (double) generator.nextInt(20);
+    		}
+    		//Pause for 4 seconds
+            try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    }
+    
+    
 }
