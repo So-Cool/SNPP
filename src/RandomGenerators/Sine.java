5b600ec16309c36f047c0e67581a134ad41e0a3e
Kacper <kacper.blazej.sokol@cern.ch>
2014-07-24 13:35:24 +0200

    Finishing sine wave.

--- src/RandomGenerators/Sine.java
+++ src/RandomGenerators/Sine.java
@@ -2,29 +2,49 @@ package RandomGenerators;
 
 import java.util.Date;
 
+import org.apache.commons.math3.distribution.PoissonDistribution;
+
 // Math.PI
 
-public class Sine {
+public class Sine implements Runnable {
 
 	private double current = 0;
+	private PoissonDistribution gen;
 	private Date timer;
 	private String genName = "Sine";
 	
-	public Sine() {
+	public Sine( int jump ) {
 		this.timer = new Date( System.currentTimeMillis() );
+		this.gen = new PoissonDistribution( jump );
 	}
 	
-	public void sinGen() {
-	
+	public void run() {
+		int timeToWait = gen.sample();
 		while( true ) {
+
 			
-			double radians = 0;
 			
 			for( double degrees = 0; degrees < 360; degrees++ ) {
-				radians = Math.toRadians(degrees);
-				current = Math.sin( radians );
+				current = Math.sin( Math.toRadians(degrees) );
+				// Print and send tick
+				System.out.println( toString() );
+				//send
 			}
+			try {
+				Thread.sleep( timeToWait * 1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 		}
 	}
+	
+	// Return string
+    @Override
+    public String toString() {
+        return "Tick! -> Generator: " + genName + " | Sample: " + current + " | Time: " + timer;
+    }
 		
 }
