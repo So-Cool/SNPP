be149fd22d6112cc4cc481347fdfafdf1a1866b5
Kacper <kacper.blazej.sokol@cern.ch>
2014-08-01 13:09:54 +0200

    Afinity code framework.

--- src/engine/Afinity.java
+++ src/engine/Afinity.java
@@ -3,28 +3,84 @@ package engine;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.espertech.esper.client.EPServiceProvider;
+
 public class Afinity {
 /////////////////////////////////////////////////////////////////////////////////////
 	// class properties //
 	
-	// expandible array for the POOL --- Java ArryaList
+	// Expandable array for the POOL --- Java ArryaList
 	private List<Double> pool = new ArrayList<Double>();
 	
-	// expandible array for memorized data = i.e. = exemplars
+	// Expandable array for memorized data = i.e. = exemplars
 	private List<Double> exemplars = new ArrayList<Double>();
 	
+	// Data for printing ??????
+	private List<Double> printable = new ArrayList<Double>();
+	
 	// Remember the current cluster count
-	private int clusters;
+	private int clusters = 1;
+	
+	// Maximal size of reservoir
+	private int resSize;
+	
+	// values for statistical tests
+	private double p;
+	
+	// ESPER engine
+	private EPServiceProvider EsperService;
 	
 	
 /////////////////////////////////////////////////////////////////////////////////////
 	// functions //
 	
-	// initialize
-	// append new point
+	// Initialize
+	public Afinity( int reservoirSize, double pValue, EPServiceProvider EPSP ) {
+		this.resSize = reservoirSize;
+		this.EsperService = EPSP;
+		this.p = pValue;
+	}
 	
-	// build
-	// check whether to rebuild
-	// rebuild
+	// Append new point
+	public void addNewPoint( double point ) {
+		// check similarity to what we already have
+		Boolean sim = simmilarity( point );
+		
+		// if similar to what we already seen add to clustering
+		if( sim ) {
+			/* FILL WITH CODE */
+		} // else add to reservoir
+		else {
+			/* FILL WITH CODE */
+		}
+		
+		// check whether model need a rebuild
+		Boolean rbld = needRebuild();
+		
+		// if rebuild needed: Just do it!
+		if (rbld)
+			rebuild();
+	}
+
+	// Check similarity to what we already have
+	private Boolean simmilarity( double newPoint ) {
+		/* FILL WITH CODE */
+		return true;
+	}
 	
+	// Check whether to rebuild
+	private Boolean needRebuild() {
+		/* FILL WITH CODE */
+		return true;
+	}
+	
+	// Rebuild the model
+	private void rebuild() {
+		/* FILL WITH CODE */
+	}
+	
+	// Get the number of clusters
+	public int getClusters() { return clusters; }
+	// Get the clustering in readable format
+	public List<Double> getPrintable() { return printable; }
 }
