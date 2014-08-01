a3773ab4a76edf09ab4f519b0b5773bec715b909
Kacper <kacper.blazej.sokol@cern.ch>
2014-08-01 11:20:14 +0200

    Clustering in progress.

--- src/engine/Afinity.java
+++ src/engine/Afinity.java
@@ -1,11 +1,20 @@
 package engine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Afinity {
 /////////////////////////////////////////////////////////////////////////////////////
 	// class properties //
 	
-	// expandible array for the POOL
+	// expandible array for the POOL --- Java ArryaList
+	private List<Double> pool = new ArrayList<Double>();
+	
 	// expandible array for memorized data = i.e. = exemplars
+	private List<Double> exemplars = new ArrayList<Double>();
+	
+	// Remember the current cluster count
+	private int clusters;
 	
 	
 /////////////////////////////////////////////////////////////////////////////////////
---
+++ src/engine/ClusterInterface.java
@@ -0,0 +1,26 @@
+package engine;
+
+public class ClusterInterface extends JFrame {
+
+	public static void main(String args[]) {
+		new HelloWorldFrame();
+	}
+	
+	package engine;
+
+	import javax.swing.JLabel;
+
+	public class HelloWorldFrame {
+
+		HelloWorldFrame() {
+			JLabel jlbHelloWorld = new JLabel("Hello World");
+			add(jlbHelloWorld);
+			this.setSize(100, 100);
+			// pack();
+			setVisible(true);
+		}
+
+	}
+
+	
+}
